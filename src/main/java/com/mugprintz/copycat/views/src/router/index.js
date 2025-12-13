13a7fb4aa20f261ab80a4053fd8b9b5033bb79ae
Lyle Smal - Onsight <lyle@onsightapp.com>
2025-12-13 11:03:01 +0200

    UI Initialization

--- src/main/java/com/mugprintz/copycat/views/src/router/index.js
+++ src/main/java/com/mugprintz/copycat/views/src/router/index.js
@@ -1,8 +1,11 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import PrivacyPolicyPage from "@/pages/PrivacyPolicyPage.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
+  routes: [
+    { path: '/privacy-policy', name: "PrivacyPolicyPage", component: PrivacyPolicyPage }
+  ],
 })
 
 export default router
--- src/main/java/com/mugprintz/copycat/views/vite.config.js
+++ src/main/java/com/mugprintz/copycat/views/vite.config.js
@@ -1,18 +1,21 @@
-import { fileURLToPath, URL } from 'node:url'
+// vite.config.js
+import { fileURLToPath, URL } from "node:url";
+import { defineConfig, loadEnv } from "vite";
+import vue from "@vitejs/plugin-vue";
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import vueDevTools from 'vite-plugin-vue-devtools'
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [
-    vue(),
-    vueDevTools(),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+  return {
+    plugins: [vue()],
+    resolve: {
+      alias: {
+        "@": fileURLToPath(new URL("./src", import.meta.url)),
+      },
     },
-  },
-})
+    server: {
+      port: Number(env.VITE_PORT) || 5173,
+      strictPort: true,
+    },
+  };
+});
