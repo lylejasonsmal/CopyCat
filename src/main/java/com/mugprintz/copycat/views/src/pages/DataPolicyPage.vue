102c24cdb74412189c246e2f2e0064ca2c7ee4c3
Lyle Smal - Onsight <lyle@onsightapp.com>
2025-12-13 11:36:13 +0200

    Policy Pages

--- src/main/java/com/mugprintz/copycat/views/src/pages/DataPolicyPage.vue
+++ src/main/java/com/mugprintz/copycat/views/src/pages/DataPolicyPage.vue
@@ -1,8 +1,10 @@
 <template>
-  <h1 id="data-policy-heading">Data Policy</h1>
-  <p id="data-policy-message">
-    Our Data Policy Aims To Ensure That Your Data Is Used Within Reasonable Boundaries And To Provided The Best Possible Service.
-  </p>
+  <div id="data-policy-page"  class="information-page">
+    <h1 id="data-policy-heading">Data Policy</h1>
+    <p id="data-policy-message">
+      Our Data Policy Aims To Ensure That Your Data Is Used Within Reasonable Boundaries And To Provided The Best Possible Service.
+    </p>
+  </div>
 </template>
 
 <style scoped>
@@ -12,3 +14,5 @@ p{
   text-align: center;
 }
 </style>
+<script setup lang="ts">
+</script>
