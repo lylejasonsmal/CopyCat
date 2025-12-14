d1178ef281e3b34b24182e6658f295c3bb06b736
Lyle Smal - Onsight <lyle@onsightapp.com>
2025-12-14 10:09:30 +0200

    UI additions

--- src/main/java/com/mugprintz/copycat/views/src/router/index.js
+++ src/main/java/com/mugprintz/copycat/views/src/router/index.js
@@ -1,12 +1,14 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import PrivacyPolicyPage from "@/pages/PrivacyPolicyPage.vue";
 import DataPolicyPage from "@/pages/DataPolicyPage.vue";
+import PrintingProgressPage from "@/pages/PrintingProgressPage.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     { path: '/privacy-policy', name: "PrivacyPolicyPage", component: PrivacyPolicyPage },
-    { path: '/data-policy', name: "DataPolicyPage", component: DataPolicyPage }
+    { path: '/data-policy', name: "DataPolicyPage", component: DataPolicyPage },
+    { path: '/printing-progress', name: "PrintingProgressPage", component: PrintingProgressPage }
   ],
 })
 
